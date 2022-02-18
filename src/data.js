8bb85dab6add2f13a63edc60e81ff5670f4cfde6
Noopur Sharma <noopurs0210@gmail.com>
2022-02-18 10:27:22 +0530

    updated data.js

--- src/data.js
+++ src/data.js
@@ -1,21 +1,21 @@
 export const sliderItems = [
     {
       id: 1,
-      img: "https://o.remove.bg/downloads/5c5ece44-1ada-451c-9c55-9ea539ee061f/photo-1559502867-c406bd78ff24-removebg-preview.png",
+      img: "../src/Images/slider-1.png",
       title: "SUMMER SALE",
       desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
       bg: "b0c4d4",
     },
     {
       id: 2,
-      img: "https://o.remove.bg/downloads/009b442d-3b7c-4538-ba11-3846d3f1a26d/photo-1572804013309-59a88b7e92f1-removebg-preview.png",
+      img: "../src/Images/slider-2.png",
       title: "AUTUMN COLLECTION",
       desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
       bg: "fcf1ed",
     },
     {
       id: 3,
-      img: "https://o.remove.bg/downloads/c09501d6-eac7-4cf3-9dc1-ccdb96693308/photo-1576209025290-d073e96b82f9-removebg-preview.png",
+      img: "../src/Images/slider-3.png",
       title: "LOUNGEWEAR LOVE",
       desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
       bg: "fbf0f4",
